6f4de404a0e4d714afd416e68e312f3157d422c2
Flávio Júnior <59364674+flavio-junior@users.noreply.github.com>
2024-12-07 16:31:16 -0300

    fix: get response error

--- compose-multiplatform/composeApp/src/commonMain/kotlin/br/com/digital/store/features/networking/utils/ToResultFlow.kt
+++ compose-multiplatform/composeApp/src/commonMain/kotlin/br/com/digital/store/features/networking/utils/ToResultFlow.kt
@@ -1,5 +1,11 @@
 package br.com.digital.store.features.networking.utils
 
+import br.com.digital.store.features.networking.utils.StatusCode.NUMBER_200
+import br.com.digital.store.features.networking.utils.StatusCode.NUMBER_299
+import br.com.digital.store.features.networking.utils.StatusCode.NUMBER_400
+import br.com.digital.store.features.networking.utils.StatusCode.NUMBER_499
+import br.com.digital.store.features.networking.utils.StatusCode.NUMBER_500
+import br.com.digital.store.features.networking.utils.StatusCode.NUMBER_599
 import io.ktor.client.call.body
 import io.ktor.client.statement.HttpResponse
 import kotlinx.coroutines.CoroutineDispatcher
@@ -13,26 +19,26 @@ inline fun <reified T> toResultFlow(
     dispatcher: CoroutineDispatcher = Dispatchers.IO,
     crossinline call: suspend () -> HttpResponse
 ): Flow<ObserveNetworkStateHandler<T>> = flow {
-    emit(ObserveNetworkStateHandler.Loading(true))
+    emit(ObserveNetworkStateHandler.Loading(l = true))
     try {
         val response = call()
         when (response.status.value) {
-            in 200..299 -> {
+            in NUMBER_200..NUMBER_299 -> {
                 val data = response.body<T>()
-                emit(ObserveNetworkStateHandler.Success(data))
+                emit(ObserveNetworkStateHandler.Success(s = data))
             }
 
-            in 400..499 -> {
-                response.body<ResponseError>()
+            in NUMBER_400..NUMBER_499 -> {
+                val errorResponse = response.body<ResponseError>()
                 emit(
                     ObserveNetworkStateHandler.Error(
                         code = response.status.value,
-                        e = response.status.description
+                        e = errorResponse.message
                     )
                 )
             }
 
-            in 500..599 -> {
+            in NUMBER_500..NUMBER_599 -> {
                 emit(
                     ObserveNetworkStateHandler.Error(
                         code = response.status.value,
