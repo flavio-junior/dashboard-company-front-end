f1d7cdb15cb59ee1ee7dc8efed54f71b901c4597
Flávio Júnior <59364674+flavio-junior@users.noreply.github.com>
2025-01-18 05:39:17 -0300

    fix: add correct name in response

--- composeApp/src/commonMain/kotlin/br/com/digital/store/features/payment/data/dto/PaymentResponseDTO.kt
+++ composeApp/src/commonMain/kotlin/br/com/digital/store/features/payment/data/dto/PaymentResponseDTO.kt
@@ -2,6 +2,7 @@ package br.com.digital.store.features.payment.data.dto
 
 import br.com.digital.store.features.order.domain.type.PaymentType
 import br.com.digital.store.features.order.domain.type.TypeOrder
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -10,9 +11,12 @@ data class PaymentResponseDTO(
     var date: String? = null,
     var hour: String? = null,
     var code: Long? = null,
+    @SerialName("type_order")
     var typeOrder: TypeOrder? = null,
+    @SerialName("type_payment")
     var typePayment: PaymentType? = null,
     var discount: Boolean? = null,
+    @SerialName("value_discount")
     var valueDiscount: Double? = null,
     var total: Double = 0.0
 )
